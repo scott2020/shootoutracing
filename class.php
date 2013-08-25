8718bbb924bc99b112f17903ff392ce6f2cdc7e4
scott2020 <scott@wolffm.com>
2013-08-25 17:15:55 -0500

    New title

--- class.php
+++ class.php
@@ -2,7 +2,7 @@
 
 <HEAD>
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
-<TITLE>Registration Detail Screen</TITLE>
+<TITLE>Registration Detail Screen New</TITLE>
 <link href="tablecloth/tablecloth.css" rel="stylesheet" type="text/css" media="screen" />
 <script type="text/javascript" src="tablecloth/tablecloth.js"></script>
 </HEAD>
