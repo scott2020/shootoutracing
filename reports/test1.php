ed30d408cf3f12eca7a8440f256f5d31909419ed
scott2020 <scott@wolffm.com>
2014-06-24 18:54:57 -0500

    globals reports
    akk globals in reports stuff

--- reports/test1.php
+++ reports/test1.php
@@ -5,6 +5,10 @@
 <body bgcolor="white">
 <?php
 
+include('../register_globals.php');
+register_globals();
+
+
 $con = mysql_connect("localhost", "shootout", "shootout")or die("Connect Error: ".mysql_error());
 
 mysql_select_db("shootout",$con);
