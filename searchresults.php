b2d70ec5157d109c48388f69ce4d24c143183ae8
scott2020 <scott@wolffm.com>
2016-05-09 19:16:03 -0500

    db host

--- searchresults.php
+++ searchresults.php
@@ -31,7 +31,7 @@ register_globals();
 
 
 
-$con = mysql_connect("localhost", $DB_USER, $DB_PASS)or die("Connect Error: ".mysql_error());
+$con = mysql_connect($DB_HOST, $DB_USER, $DB_PASS)or die("Connect Error: ".mysql_error());
 mysql_select_db($DB_NAME,$con);
 
 #   $con = mysql_connect("localhost", "shootout", "shootout")or die("Connect Error: ".mysql_error());
