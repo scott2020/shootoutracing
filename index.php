c0c2b5b79b2764082b98af4d9ab046dd7bd932a9
scott2020 <scott@wolffm.com>
2016-07-27 16:35:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<?php include './config.php';?>
+<?php include '/root/config.php';?>
 <title><?php echo $SITE_NAME;?></title>
 </head>
 
