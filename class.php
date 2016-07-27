688a88ea378da0f50d85d2ce2070aa7fb82fd94d
scott2020 <scott@wolffm.com>
2016-07-27 16:36:18 -0500

    Update class.php

--- class.php
+++ class.php
@@ -26,7 +26,7 @@ If you do not see the class you need to register, enter it in the box and click 
 </form>
 
 <?php
-include './config.php';
+include '/root/config.php';
 include('register_globals.php');
 register_globals();
 
