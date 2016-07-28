37f1a60efd67bb57d1749a885fb3086ff0fa4c13
scott2020 <scott@wolffm.com>
2016-07-28 11:20:59 -0500

    Update index.php

--- reports/index.php
+++ reports/index.php
@@ -5,7 +5,7 @@
 <meta name="GENERATOR" content="Microsoft FrontPage 5.0">
 <meta name="ProgId" content="FrontPage.Editor.Document">
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<?php include '../config.php';?>
+<?php include '/config/config.php';?>
 <title>Reports - <?php echo $SITE_NAME;?></title>
 </head>
 
