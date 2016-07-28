af08883af3636f3b548706a7681fedddb16db2d0
scott2020 <scott@wolffm.com>
2016-07-28 11:11:58 -0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@
 <meta name="GENERATOR" content="Microsoft FrontPage 5.0">
 <meta name="ProgId" content="FrontPage.Editor.Document">
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<?php include '../config.php';
+<?php include '/config/config.php';
 include('../register_globals.php');
 register_globals();
 ?>
