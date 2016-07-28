ae04cea6b40e543176509a3a42b2bd388322aca7
scott2020 <scott@wolffm.com>
2016-07-28 12:58:39 -0500

    Create b2.php

---
+++ b2.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ozarkemail -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:320px;height:100px"
+     data-ad-client="ca-pub-3815362766482748"
+     data-ad-slot="8897423111"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
